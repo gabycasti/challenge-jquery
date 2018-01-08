36726f85849721923be234236d8b22860a26f2c9
gabycasti <castillg@gmail.com>
2018-01-08 10:11:48 -0300

    primer punto finalizado

--- js/script-index.js
+++ js/script-index.js
@@ -42,4 +42,10 @@ function renderActivity(recipe) {
 	
 }
 
-
+var path = window.location.pathname.split("/");
+var pagina = path[path.length-1];
+if(pagina==='index.html'){
+    $('.js-back').hide();
+}else if(pagina==='recipe.html'){
+    $('.js-menu').hide();
+}
