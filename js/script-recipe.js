0a021797c4cda0137dd99e22b182d421c69d2a2b
gabycasti <castillg@gmail.com>
2018-01-08 23:30:09 -0300

    punto siete

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,5 +1,5 @@
 $(document).ready( function(){
-
+ // Creando documento 
    $('#btn-secondary js-show-recipe').click(function(){
       ('#btn-secondary js-show-recipe').addClass('active');
       ('#btn-secondary js-show-make').removeClass('active');
